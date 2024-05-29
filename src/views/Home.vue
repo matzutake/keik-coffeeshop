5f4c97299a4a35144ec00c25ce416053f4ac28a8
pahmutov <pahmutov@f5.com.ru>
2024-05-29 18:45:33 +0300

    upd

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <div>Кейк</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #2b2a28;
+  color: #ffffff;
+  font-size: 90px;
+  letter-spacing: 0.1em;
+}
+</style>
